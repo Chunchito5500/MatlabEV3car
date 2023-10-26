f796bb7b291a4ed2350a34ccdbcfe78b7a1c611b
Chunchito5500 <64931996+Chunchito5500@users.noreply.github.com>
2023-10-26 14:50:11 -0700

    Add files via upload

---
+++ OtherMain.m
@@ -0,0 +1,48 @@
+color = brick.ColorCode(1);
+distance = brick.UltrasonicDist(2);
+button = brick.TouchPressed(3);
+
+%% RIGHT A, LEFT B
+
+	while color ~= 3    %Green
+
+		pause(0.2);
+
+		if ((color == 1) || (color == 4))     %Black Or Yellow
+			brick.MoveMotor('AB', -30);
+		end
+
+		if color == 5     %Red
+			brick.StopMotor('AB')
+            pause(3);
+            brick.MoveMotor('AB', -30)
+			pause(1);
+		end
+
+		if distance >= 40 %If car is 30 or less centimeters from a wall
+            pause(1.5)
+			brick.StopMotor('AB')
+            pause(1)
+            brick.MoveMotorAngleAbs('A', -70, 90, 'Brake')
+            brick.WaitForMotor('A')
+            brick.MoveMotor('AB', -30)
+            pause(1.5)
+		end
+
+		if button %if button was pressed
+			brick.StopMotor('AB')
+			pause(1);
+			brick.MoveMotor('AB', 20)
+            pause(1.5)
+            brick.StopMotor('AB')
+			brick.MoveMotorAngleAbs('B', -70, 90, 'Brake')
+            brick.WaitForMotor('B')
+        end
+			
+		color = brick.ColorCode(1);
+		distance = brick.UltrasonicDist(2);
+        button = brick.TouchPressed(3);
+
+	end
+
+disp('You Have Reached Your Destination');
