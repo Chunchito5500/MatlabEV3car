a0ae86130874c7dc34a5e12222e1deb16a5965bb
Basel Kurian <64931996+Chunchito5500@users.noreply.github.com>
2023-11-14 17:57:03 -0700

    Delete motorss.m

--- motorss.m
+++
@@ -1,7 +0,0 @@
-brick.MoveMotor('AB', 50)
-brick.StopMotor('AB', Brake)
-brick.ResetMotorAngle('B');
-brick.MoveMotorAngleAbs('B', 25, 90, 'Brake');
-brick.WaitForMotor('B');
-brick.MoveMotor('AB', 50)
-
