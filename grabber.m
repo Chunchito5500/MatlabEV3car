aac9c878865b4f90be9fc5075313b5c178c66dec
Basel Kurian <64931996+Chunchito5500@users.noreply.github.com>
2023-11-14 17:56:51 -0700

    Delete grabber.m

--- grabber.m
+++
@@ -1,8 +0,0 @@
-%%brick.MoveMotor('C', 20);
-%%pause(5);
-%%brick.StopMotor('C'); 
-
-%%brick.ResetMotorAngle('C'); 
-%%brick.MoveMotorAngleAbs('C', 10, 25, 'Brake'); 
-brick.MoveMotor('AB', 50);
-brick.StopMotor('AB'); 
